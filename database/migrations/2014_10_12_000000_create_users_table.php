50b1c1a2842603a0254b5126d05c3e69b42b5acf
meWasim <matainja0120@gmail.com>
2024-04-06 00:44:02 +0530

    debug

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             'users', function (Blueprint $table){
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('user_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
